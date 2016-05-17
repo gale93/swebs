38fa809d176a88ce89e07e721d23680fdae6a06b
Vasile Botoroga <vbotoroga@gmail.com>
2016-05-17 15:39:17 +0200

    fixed absolute path bug

--- filehandler/dirmaker.go
+++ filehandler/dirmaker.go
@@ -3,22 +3,29 @@ package filehandler
 import (
 	"fmt"
 	"os"
+	"path/filepath"
 )
 
 // SetUp will create the needed directories
 func SetUp() error {
 
-	errMsg := "Impossible create resources dir. Consider -checkdir=false.\n"
+	errMsg := "Cannot create resources dir. Consider -checkdir=false.\n"
 
-	if err := os.MkdirAll("resources", 0711); err != nil {
+	// get current path
+	path, error := filepath.Abs(filepath.Dir(os.Args[0]))
+	if error != nil {
+		return fmt.Errorf(errMsg + "[error] : " + error.Error())
+	}
+
+	if err := os.MkdirAll(path+"/resources", 0711); err != nil {
 		return fmt.Errorf(errMsg + "[error] : " + err.Error())
 	}
 
-	if err := os.MkdirAll("resources/json", 0711); err != nil {
+	if err := os.MkdirAll(path+"/resources/json", 0711); err != nil {
 		return fmt.Errorf(errMsg + "[error] : " + err.Error())
 	}
 
-	f, err := os.Create("resources/index.html")
+	f, err := os.Create(path + "/resources/index.html")
 	if err != nil {
 		return fmt.Errorf(errMsg + "[error] : " + err.Error())
 	}
